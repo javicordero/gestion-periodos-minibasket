8d2787aa1f1f48da26150b36cfac98631b7417ca
Javier Cordero <javiercorderotoscano@gmail.com>
2023-04-26 20:33:37 +0200

    feat: "Router implemented, LoginComponent created"

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
+import { AppRoutingModule } from '../app-routing.module';
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AppRoutingModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
